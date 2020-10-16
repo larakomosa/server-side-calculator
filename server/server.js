203b6bac77eaa532a944ae9db1b6521a878ce408
Lara Komosa <larakomosa@gmail.com>
2020-10-16 18:02:55 -0500

    post working for x,y,o

--- server/server.js
+++ server/server.js
@@ -5,19 +5,54 @@ const PORT = 5000;
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('public'));
 
 // GET & POST Routes go here
-//app.post('/number', (req, res) => {
-//ourGuesses.push(req.body);
-  //res.sendStatus(201);})
+app.post('/math', (req, res) => {
+  // calculations
+  // {
+  //   calculationInputs: [
+  //     {
+  //       x:5,
+  //       y:7,
+  //       o:'*'
+  //     }
+  //   ]
+  // }
+const calculations = req.body;
+console.log('calculations:', calculations );
+res.sendStatus(201);});
 
-//app.get('/number', (req, res) => {
-  //res.send(kittyKat);
-//});
+
+
+app.get('/calculator', (req, res) => {
+  res.send("hello!");
+});
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 });
+
+function multiplyNumbers(x,y){
+  return x*y;
+}
+
+function divideNumbers(x,y){
+  return x/y;
+}
+
+function subtractNumbers(x,y){
+  return x-y;
+}
+
+function addNumbers(x,y){
+  return x+y;
+}
+
+console.log(multiplyNumbers(3,5));
+console.log(divideNumbers(3,5));
+console.log(subtractNumbers(3,5));
+console.log(addNumbers(3,5));
