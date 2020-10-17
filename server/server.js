f4394715c5b1a27dc000f9c9e28b6ce3d105a411
Lara Komosa <larakomosa@gmail.com>
2020-10-16 19:37:20 -0500

    array created

--- server/server.js
+++ server/server.js
@@ -3,39 +3,6 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
-// This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-
-// Serve up static files (HTML, CSS, Client JS)
-app.use(express.static('public'));
-
-// GET & POST Routes go here
-app.post('/math', (req, res) => {
-  // calculations
-  // {
-  //   calculationInputs: [
-  //     {
-  //       x:5,
-  //       y:7,
-  //       o:'*'
-  //     }
-  //   ]
-  // }
-const calculations = req.body;
-console.log('calculations:', calculations );
-res.sendStatus(201);});
-
-
-
-app.get('/calculator', (req, res) => {
-  res.send("hello!");
-});
-
-app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
-});
-
 function multiplyNumbers(x,y){
   return x*y;
 }
@@ -51,6 +18,37 @@ function subtractNumbers(x,y){
 function addNumbers(x,y){
   return x+y;
 }
+
+// This must be added before GET & POST routes.
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+// Serve up static files (HTML, CSS, Client JS)
+app.use(express.static('public'));
+
+// GET & POST Routes go here
+//{   
+ // "x": 9,
+  //"y": 5,
+ // "c": "*"
+//}
+
+app.post('/math', (req, res) => {
+const calculations = req.body;
+console.log('calculations:', calculations );
+const answers = []; //calculations
+res.sendStatus(200);
+});
+
+app.get('/calculator', (req, res) => {
+  res.send("hello!");
+});
+
+app.listen(PORT, () => {
+  console.log ('Server is running on port', PORT)
+});
+
+
 
 console.log(multiplyNumbers(3,5));
 console.log(divideNumbers(3,5));
