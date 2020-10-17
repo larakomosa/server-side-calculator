03214ac3398c6e82dfc3dff5b2d657ec27644c26
Lara Komosa <larakomosa@gmail.com>
2020-10-17 17:20:24 -0500

    results array created

--- server/server.js
+++ server/server.js
@@ -3,35 +3,12 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
-function multiplyNumbers(x,y){
-  return x*y;
-}
-
-function divideNumbers(x,y){
-  return x/y;
-}
-
-function subtractNumbers(x,y){
-  return x-y;
-}
-
-function addNumbers(x,y){
-  return x+y;
-}
-
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-// Serve up static files (HTML, CSS, Client JS)
-app.use(express.static('public'));
 
-// GET & POST Routes go here
-//{   
- // "x": 9,
-  //"y": 5,
- // "c": "*"
-//}
+app.use(express.static('public'));
 
 app.post('/math', (req, res) => {
 const calc = req.body.math
@@ -42,27 +19,27 @@ const answers = []; // results from calculator
 //const math = calculations[i];
   if (calc.operator === '+') {
     calc.answer = Number(calc.x) + Number(calc.y);
+    answers.push(calc.x, calc.operator, calc.y, "=", calc.answer)
   } else if (calc.operator === '-') {
     calc.answer = Number(calc.x) + Number(calc.y);
+    answers.push(calc.x, calc.operator, calc.y, "=", calc.answer)
   } else if (calc.operator === '*') {
     calc.answer = Number(calc.x) + Number(calc.y);
+    answers.push(calc.x, calc.operator, calc.y, "=", calc.answer)
   } else if (calc.operator === '/') {
     calc.answer = Number(calc.x) + Number(calc.y)
+    answers.push(calc.x, calc.operator, calc.y, "=", calc.answer)
   }
-  console.log(calc.x, calc.operator, calc.y, "+", calc.answer);
-  answers.push(calc.answer)
+  console.log(calc.x, calc.operator, calc.y, "=", calc.answer);
   res.sendStatus(200);
 })
 
 app.get('/calculator', (req, res) => {
+  const results = calc.answer
+  res.send(results)
   console.log('hi')
 });
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 });
-
-console.log(multiplyNumbers(3,5));
-console.log(divideNumbers(3,5));
-console.log(subtractNumbers(3,5));
-console.log(addNumbers(3,5));
