33fc845bd5588f2f295127dd90c9fa0076651e3b
Lara Komosa <larakomosa@gmail.com>
2020-10-18 11:54:54 -0500

    cleaned up spacing, adding field requirements

--- public/client.js
+++ public/client.js
@@ -1,93 +1,99 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  console.log('jquery is loaded!')
-  $(".submit").on('click',calculate);
-  $('.js-add').on('click', conversion)
-  $('.js-subtract').on('click', conversion)
-  $('.js-multiply').on('click', conversion)
-  $('.js-divide').on('click', conversion)
-  $('.clear').on('click', clearCurrent)
-};
+  console.log('jquery is loaded!');
+  $('.submit').on('click', calculate);
+  $('.js-add').on('click', conversion);
+  $('.js-subtract').on('click', conversion);
+  $('.js-multiply').on('click', conversion);
+  $('.js-divide').on('click', conversion);
+  $('.clear').on('click', clearCurrent);
+}
 
 let operator = '';
-function conversion(){
-operator = $(this).prop('value');
-console.log(operator);
-return operator;
+function conversion() {
+  operator = $(this).prop('value');
+  console.log(operator);
+  return operator;
 }
 
 function calculate() {
-    const calculations = 
-      {
+  if (
+    $.trim($('.field-number-x').val()) === '' ||
+    $.trim($('.field-number-y').val()) === ''
+  ) {
+    alert('Please add numbers to both input fields');
+    return false;
+  } else {
+    const calculations = {
       x: $('.field-number-x').val(),
       y: $('.field-number-y').val(),
       operator: operator,
-      }
-      $.ajax({
-        type: 'POST',
-        url: '/math',
-        data: {math: calculations}})
-        .then(function (response) {
-          console.log('POST Message:', response);
-          // GET -> results
-          giveAnswers();
-          answerList() 
-        })
-        .catch(function (err) {
-          console.log(err);
-          alert('Error- Post Side');
-        })
-    console.log(calculations);
-    }
-
-
-    function giveAnswers() {
-      $.ajax({
-        type: 'GET',
-        url: '/calculator',
+    };
+    $.ajax({
+      type: 'POST',
+      url: '/math',
+      data: { math: calculations },
+    })
+      .then(function (response) {
+        console.log('POST Message:', response);
+        // GET -> results
+        giveAnswers();
+        answerList();
       })
-        .then(function (response) {
-          renderAnswerDisplay(response);
-          console.log('GET1 answer:', response);
-        })
-        .catch(function (err) {
-          console.log(err);
-          alert('IT BROKE');
-        })}
-
-    function renderAnswerDisplay(result) {
-    const results = $('.js-calculations');
-    results.text(result);
+      .catch(function (err) {
+        console.log(err);
+        alert('Error- Post Side');
+      });
+  }
 }
 
-        function answerList() {
-          $.ajax({
-            type: 'GET',
-            url: '/calculator/list',
-          })
-          .then(function (response) {
-          renderAnswers(response);
-          console.log('GET2 answer', response);
-        })
-        .catch(function (err) {
-          console.log(err);
-          alert('IT BROKE');
-        })}
-          
-      
-          
-        function renderAnswers(list) {
-        $('.array').empty();
-        for (let i = 0; i < list.length; i++) {   
-          $('.array').append(`
+function giveAnswers() {
+  $.ajax({
+    type: 'GET',
+    url: '/calculator',
+  })
+    .then(function (response) {
+      renderGiveAnswers(response);
+      console.log('GET1 answer:', response);
+    })
+    .catch(function (err) {
+      console.log(err);
+      alert('IT BROKE');
+    });
+}
+
+function renderGiveAnswers(result) {
+  const results = $('.js-calculations');
+  results.text(result);
+}
+
+function answerList() {
+  $.ajax({
+    type: 'GET',
+    url: '/calculator/list',
+  })
+    .then(function (response) {
+      renderAnswerList(response);
+      console.log('GET2 answer', response);
+    })
+    .catch(function (err) {
+      console.log(err);
+      alert('IT BROKE');
+    });
+}
+
+function renderAnswerList(list) {
+  $('.array').empty();
+  for (let i = 0; i < list.length; i++) {
+    $('.array').append(`
          <li>${list[i]}</li>
-          `);}}
-            
-         function clearCurrent(answer){
-         $('.field-number-x').val(''),
-         $('.field-number-y').val(''),
-         $('.js-calculations').empty();
-         }
-      
-     
+          `);
+  }
+}
+
+function clearCurrent(answer) {
+  $('.field-number-x').val(''),
+    $('.field-number-y').val(''),
+    $('.js-calculations').empty();
+}
--- server/server.js
+++ server/server.js
@@ -1,47 +1,38 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 let answer;
 let sequence;
-let answers= [];
-let calc;
+let answers = [];
 
-// This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-
 app.use(express.static('public'));
 
 app.post('/math', (req, res) => {
-let calc = req.body.math;
-console.log('calculations:', calc );
-// results from calculator
-//for (let i =0; i<calculations.length; i++){
-//const math = calculations[i];
+  let calc = req.body.math;
+  console.log('calculations:', calc);
   if (calc.operator === '+') {
     answer = Number(calc.x) + Number(calc.y);
-    sequence = (calc.x + "+" + calc.y + "=" + answer);
-    answers.push(sequence) 
+    sequence = calc.x + ' ' + '+' + ' ' + calc.y + ' ' + '=' + ' ' + answer;
+    answers.push(sequence);
   } else if (calc.operator === '-') {
     answer = Number(calc.x) - Number(calc.y);
-    sequence = (calc.x + "-" + calc.y + "=" + answer);
-    answers.push(sequence) 
-  }
-  else if (calc.operator === '*') {
+    sequence = calc.x + ' ' + '-' + ' ' + calc.y + ' ' + '=' + ' ' + answer;
+    answers.push(sequence);
+  } else if (calc.operator === '*') {
     answer = Number(calc.x) * Number(calc.y);
-    sequence = (calc.x + "*" + calc.y + "=" + answer);
-    answers.push(sequence) 
-  } else if (calc.operator === '/') {
-    answer = Number(calc.x) / Number(calc.y);
-    sequence = (calc.x + "/" + calc.y + "=" + answer);
-    answers.push(sequence) 
+    sequence = calc.x + ' ' + '*' + ' ' + calc.y + ' ' + '=' + ' ' + answer;
+    answers.push(sequence);
+  } else if (calc.operator === '÷') {
+    answer.toFixed(2) = Number(calc.x) / Number(calc.y);
+    sequence =
+      calc.x + ' ' + '÷' + ' ' + calc.y + ' ' + '=' + ' ' + answer.toFixed(2);
+    answers.push(sequence);
   }
-  console.log("answer",answer);
-  console.log("sequence",sequence);
   res.sendStatus(200);
-})
+});
 
 app.get('/calculator', (req, res) => {
   let giveAnswers = [answer];
@@ -53,5 +44,5 @@ app.get('/calculator/list', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
+  console.log('Server is running on port', PORT);
 });
